9b55c56a60d47f47e8900cf7c9731c40f60a3541
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-08-04 10:11:11 +0530

    m

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-//
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-  const [text, setText] = useState('');
-
+  const [text, setText] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/todos')
-    .then(res => {
-      setTodos(res.data);
-    })
-    .catch(err => console.log('Error fetching todos:', err));
-  }, []); 
+    axios
+      .get("http://localhost:5000/api/todos")
+      .then((res) => {
+        setTodos(res.data);
+      })
+      .catch((err) => console.log("Error fetching todos:", err));
+  }, []);
 
   const addTodo = () => {
-    axios.post('http://localhost:5000/api/todos', {
-      text
-    })
-    .then(res => {
-      setTodos([...todos, res.data]);
-      setText('');
-    })
-    .catch(err => console.log('Error adding todo:', err));
+    axios
+      .post("http://localhost:5000/api/todos", {
+        text,
+      })
+      .then((res) => {
+        setTodos([...todos, res.data]);
+        setText("");
+      })
+      .catch((err) => console.log("Error adding todo:", err));
   };
 
   return (
-    <div >
+    <div className="">
       <h1>Todo_App</h1>
-      <input type="text" value={text} onChange={e => setText(e.target.value)} />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <button onClick={addTodo}>Add_Todo</button>
       <ul>
-         {todos.map(todo => (
-            <li key={todo.id}>
-              {todo.text}
-            </li>
-         ))}
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
       </ul>
-    </div> 
+    </div>
   );
-
-
-}
+};
 
 export default App;
